43bbc9076067d638cfe3a03e106aa1d05aa4319c
Bob Karreman <bob@karreman.org>
2012-01-16 17:41:17 +0100

    Made Tappable work with checkboxes and labels

--- jquery.tappable.js
+++ jquery.tappable.js
@@ -58,94 +58,106 @@
  *
  */
 
-;(function($) {
-  var touchSupported = ('ontouchstart' in window)
+(function($) {
+  var touchSupported = ('ontouchstart' in window);
 
   $.fn.tappable = function(options) {
     var cancelOnMove = true,
-        onlyIf = function() { return true },
+        onlyIf = function() { return true; },
         touchDelay = 0,
-        callback
+        callback;
 
     switch(typeof options) {
       case 'function':
-        callback = options
+        callback = options;
         break;
       case 'object':
-        callback = options.callback
+        callback = options.callback;
 
         if (typeof options.cancelOnMove != 'undefined') {
-          cancelOnMove = options.cancelOnMove
+          cancelOnMove = options.cancelOnMove;
         }
 
         if (typeof options.onlyIf != 'undefined') {
-          onlyIf = options.onlyIf
+          onlyIf = options.onlyIf;
         }
 
         if (typeof options.touchDelay != 'undefined') {
-          touchDelay = options.touchDelay
+          touchDelay = options.touchDelay;
         }
 
+        break;
+      default:
         break;
     }
 
     var fireCallback = function(el, event) {
       if (typeof callback == 'function' && onlyIf(el)) {
-        callback.call(el, event)
+        callback.call(el, event);
       }
-    }
+    };
 
     if (touchSupported) {
       this.bind('touchstart', function(event) {
-        var el = this
+        var el = this;
 
         if (onlyIf(this)) {
-          $(el).addClass('touch-started')
+          $(el).addClass('touch-started');
 
           window.setTimeout(function() {
             if ($(el).hasClass('touch-started')) {
-              $(el).addClass('touched')
+              $(el).addClass('touched');
             }
-          }, touchDelay)
+          }, touchDelay);
         }
 
-        return true
-      })
+        return true;
+      });
 
       this.bind('touchend', function(event) {
-        var el = this
+        var el = this;
 
         if ($(el).hasClass('touch-started')) {
-          $(el)
-            .removeClass('touched')
-            .removeClass('touch-started')
+          $(el).removeClass('touched').removeClass('touch-started');
 
-          fireCallback(el, event)
+          if ( $(event.target).is('input[type="checkbox"]') ) {
+            $(event.target).attr('checked', !$(event.target).is(':checked') );
         }
 
-        return true
-      })
+          if ( $(event.target).is('label') ) {
+            var forId = $(event.target).attr('for');
+            var forEl = $('#' + forId);
+            if (forEl.is(':checkbox')) {
+              forEl.attr('checked', !forEl.is(':checked') );
+            } else {
+              forEl.focus();
+            }
+          }
+
+          fireCallback(el, event);
+        }
+
+        return true;
+      });
 
       this.bind('click', function(event) {
-        event.preventDefault()
-      })
+          event.preventDefault();
+      });
 
       if (cancelOnMove) {
         this.bind('touchmove', function() {
-          $(this)
-            .removeClass('touched')
-            .removeClass('touch-started')
-        })
+          $(this).removeClass('touched').removeClass('touch-started');
+        });
       }
     } else if (typeof callback == 'function') {
       this.bind('click', function(event) {
         if (onlyIf(this)) {
-          callback.call(this, event)
+          callback.call(this, event);
         }
-      })
+      });
     }
 
-    return this
-  }
+    return this;
+  };
 })(jQuery);
 
